93177af2b398259d7ee786c9d4f9fa96eaae5ac5
Hubert51 <ruijiegeng@gmail.com>
2017-09-03 22:31:45 -0400

    Add another module to crawl job information in bossZhipin website

---
+++ bossZhipinSample/__init__.py
@@ -0,0 +1,1 @@
+from .bossZhipinSample import bossZhipinMethod
--- test/basicTest.py
+++ test/basicTest.py
@@ -3,8 +3,17 @@ import time
 
 start = time.time()
 
-company = input("Please enter the name of comany -> ")
-jobList = lagouSample.lagou.lagouMethod(company)
+# company = input("Please enter the name of comany -> ")
+jobList = lagouSample.lagou.lagouMethod("探探")
+
+end = time.time()
+
+print("The time usage to find job in {:s} is {:f} with timeout 20s".format(company,(end-start)))
+
+start = time.time()
+
+# company = input("Please enter the name of comany -> ")
+jobList = lagouSample.lagou.lagouMethod("百度")
 
 end = time.time()
 
